802fbd1ce116db60182de368c4b864d8db09ff4d
Riza Fahmi <rizafahmi@gmail.com>
2017-03-28 11:07:13 +0700

    Finish dispatch and action

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const Redux = require('redux')
 
-const reducer = () => {
-  return 0
+// Reducer
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
 }
 
-const store = Redux.createStore(reducer)
+const store = Redux.createStore(counterReducer)
 
-console.log(store.getState())
+// Actions
+const incrementCounter = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrementCounter = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+console.log('Initial state: ', store.getState())
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+console.log('Current state: ', store.getState())
